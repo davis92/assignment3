8cedb29b921249975b9ae1db95b28f51e542f25a
davisnguyen <davisnguyen@davisnguyensmbp.wireless.yale.internal>
2014-11-05 12:45:22 -0500

    Part I finished

--- CPSC112_Assignment3/src/edu/yale/cpsc112_assignment3/CPSC112_Assignment3.java
+++ CPSC112_Assignment3/src/edu/yale/cpsc112_assignment3/CPSC112_Assignment3.java
@@ -18,6 +18,32 @@ public class CPSC112_Assignment3 {
 
   public static void makeMySecret() {
      // Part 1 code goes here (please leave the next few lines at the end of the makeMySecret method)
+	 
+	  int[] secret = new int[4];
+	  
+	  secret[0] = 0;
+	  secret[1] = 0;
+	  secret[2] = 0;
+	  secret[3] = 0;
+	  
+	  secret[0] = 1 + (int)(Math.random()*7);
+	  
+	  do{
+	  secret[1] = 1 + (int)(Math.random()*7);
+	  } while (secret[0]==secret[1]);
+	  
+	  do{
+	  secret[2] = 1 + (int)(Math.random()*7);
+	  } while (secret[0]==secret[2]||secret[1]==secret[2]);
+	  
+	  do{
+	  secret[3] = 1 + (int)(Math.random()*7);
+	  } while (secret[0]==secret[3]||secret[1]==secret[3]||secret[2]==secret[3]);
+	  
+	  String mySecret = ""+ secret[0] + "" + ""+ secret[1] + "" + ""+ secret[2] + "" +""+ secret[3] + "";
+	  
+	  System.out.println(mySecret);
+	  
     if (DEBUG)
     {
        System.out.println(mySecret);
