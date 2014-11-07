5c11693f3ffb7c8f4a2bfd07fd35f8ec9807f72d
davisnguyen <davisnguyen@davisnguyensmbp.wireless.yale.internal>
2014-11-06 23:18:37 -0500

    Part III almost done. Remove Booleans

--- CPSC112_Assignment3/src/edu/yale/cpsc112_assignment3/CPSC112_Assignment3.java
+++ CPSC112_Assignment3/src/edu/yale/cpsc112_assignment3/CPSC112_Assignment3.java
@@ -8,9 +8,14 @@ public class CPSC112_Assignment3 {
   public static boolean DEBUG = true;
   public static Random r = new Random();
 
+  public static int a = 0; //chapter on global variable
+  public static int b = 0;
+  public static int c = 0;
+  public static int d = 0;
+  
   public static void main(String[] args) {
     makeMySecret();
-    isGameOver("a234");
+    isGameOver("1234");
     isGameOver("4321");
     isGameOver("2567");
     isGameOver("1432");
@@ -18,29 +23,22 @@ public class CPSC112_Assignment3 {
 
   public static void makeMySecret() {
      // Part 1 code goes here (please leave the next few lines at the end of the makeMySecret method)
-	 
-	  int[] secret = new int[4];
-	  
-	  secret[0] = 0;
-	  secret[1] = 0;
-	  secret[2] = 0;
-	  secret[3] = 0;
-	  
-	  secret[0] = 1 + (int)(Math.random()*7);
+	 	  	  
+	  a = 1 + r.nextInt(7);
 	  
 	  do{
-	  secret[1] = 1 + (int)(Math.random()*7);
-	  } while (secret[0]==secret[1]);
+	  b = 1 + r.nextInt(7);
+	  } while (a==b);
 	  
 	  do{
-	  secret[2] = 1 + (int)(Math.random()*7);
-	  } while (secret[0]==secret[2]||secret[1]==secret[2]);
+	  c = 1 + r.nextInt(7);
+	  } while (a==c|| b==c);
 	  
 	  do{
-	  secret[3] = 1 + (int)(Math.random()*7);
-	  } while (secret[0]==secret[3]||secret[1]==secret[3]||secret[2]==secret[3]);
+	  d = 1 + r.nextInt(7);
+	  } while (a==d||b==d||c==d);
 	  
-	  String mySecret = ""+ secret[0] + "" + ""+ secret[1] + "" + ""+ secret[2] + "" +""+ secret[3] + "";
+	  String mySecret = ""+ a + "" + ""+ b + "" + ""+ c + "" +""+ d + "";
 	  
 	  
     if (DEBUG)
@@ -69,14 +67,120 @@ public class CPSC112_Assignment3 {
   }
 
   public static boolean isGameOver(String input) 
-  {
-	  int match = 0;
-	  int place = 0;
-	  isGuessValid (input);
-	  
+  {  
 	  if (isGuessValid(input) == true)
 	  {
+		  int match = 0;
+		  int place = 0;
 		  
+		  char w = input.charAt(0);
+		  char x = input.charAt(1);
+		  char y = input.charAt(2);
+		  char z = input.charAt(3);
+		  
+		  if (a == w )
+		  {
+		  boolean aw = true;
+		  place++;
+		  }
+		  
+		  if (a == x )
+		  {
+		  boolean ax = true;
+		  }
+		  
+		  if (a == y)
+		  {
+		  boolean ay = true;
+		  }
+		  
+		  if (a == z)
+		  {
+		  boolean az = true;
+		  }
+
+		  // b time
+		  
+		  if (b == w )
+		  {
+		  boolean bw = true;
+		  }
+		  
+		  if (b == x )
+		  {
+		  boolean bx = true;
+		  place++;
+		  }
+		  
+		  if (b == y)
+		  {
+		  boolean by = true;
+		  }
+		  
+		  if (b == z)
+		  {
+		  boolean bz = true;
+		  }  
+		  
+		  // c
+		  
+		  if (c == w )
+		  {
+		  boolean cw = true;
+		  }
+		  
+		  if (c == x )
+		  {
+		  boolean cx = true;
+		  }
+		  
+		  if (c == y)
+		  {
+		  boolean cy = true;
+		  place++;
+		  }
+		  
+		  if (c == z)
+		  {
+		  boolean cz = true;
+		  }  
+		  
+		  //d
+		  
+		  if (d == w )
+		  {
+		  boolean dw = true;
+		  }
+		  
+		  if (d == x )
+		  {
+		  boolean dx = true;
+		  }
+		  
+		  if (d == y)
+		  {
+		  boolean dy = true;
+		  }
+		  
+		  if (d == z)
+		  {
+		  boolean dz = true;
+		  place++;
+		  }  
+		  
+
+			  
+		  match = 1;
+		  match = 2;
+		  match = 3;
+		  match = 4;
+				 
+		  place = 1;
+		  place = 2;
+		  place = 3;
+		  place = 4; 
+
+		
 		  
 	  System.out.println("Your Guess is: " + input + " Your Result is " + match + "," + place);
 	  return true;
